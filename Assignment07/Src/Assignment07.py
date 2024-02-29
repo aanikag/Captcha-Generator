c9668dce890f7e74d26240cb8939154de9d221ba
aanikagarre <aanikagarre@localhost>
2024-02-29 00:32:45 -0500

    First commit

--- Assignment07/Src/Assignment07.py
+++ Assignment07/Src/Assignment07.py
@@ -1,3 +1,4 @@
+#Assignment.py
 '''
 Created on Feb 26, 2020
 
@@ -27,27 +28,34 @@ def draw_random_ellipse(draw):
                                   default_color_blue + random.randrange(-100,100,1), 255), 
                                   outline = "black")
 
-def generate_captcha():
+def generate_captcha(num_characters, filename):
     '''
     Generate a captcha
+    @param: num_characters: Number of characters in the captcha string (6-10)
+    @param: filename: filename of captcha
     :return: A tuple (image, captcha string encoded in the image)
     '''
-    captcha_string = generate_random_string(5)
-#   print(">" + captcha_string + "<")
-    captcha_image = Image.new("RGBA", (400, 200), (default_color_red,default_color_green,default_color_blue))
-    draw = ImageDraw.Draw(captcha_image, "RGBA")
-    for i in range(1,20):
-        draw_random_ellipse(draw)
-
-    fontStyle = ImageFont.truetype("Aaargh.ttf", 48)     # font must be in the same folder as the .py file. 
-
-    # Arbitrary starting co-ordinates for the text we will write
-    x = 10 + random.randrange(0, 100, 1)
-    y = 79 + random.randrange(-10, 10, 1)
-    for letter in captcha_string:
-#       print(letter)
-        draw.text((x, y), letter, (0,0,0),font=fontStyle)    # Write in black
-        x = x + 35
-        y = y +  random.randrange(-10, 10, 1)
+    if not (6 >= num_characters <= 10):
+        captcha_string = generate_random_string(5)
+    #   print(">" + captcha_string + "<")
+        captcha_image = Image.new("RGBA", (400, 200), (default_color_red,default_color_green,default_color_blue))
+        draw = ImageDraw.Draw(captcha_image, "RGBA")
+        for i in range(1,20):
+            draw_random_ellipse(draw)
+            
+        fonts = ["Aaargh.ttf", "CenturyGothic.ttf", "Mistral.ttf","UniverseCondensed.ttf"]
     
-    return (captcha_image, captcha_string)  # return a heterogeneous tuple
+    
+        # Arbitrary starting co-ordinates for the text we will write
+        x = 10 + random.randrange(0, 100, 1)
+        y = 79 + random.randrange(-10, 10, 1)
+        for letter in captcha_string:
+    #       print(letter)
+            fontStyle = ImageFont.truetype(random.choice(fonts), 48) # font must be in the same folder as the .py file. 
+            draw.text((x, y), letter, (0,0,0),font=fontStyle)    # Write in black
+            x = x + 35
+            y = y +  random.randrange(-10, 10, 1)
+        
+        return (captcha_image, captcha_string)  # return a heterogeneous tuple
+    else:
+        raise ValueError ("Number of characters must be between 6 and 10 (inclusive)")
--- Assignment07/Src/Main.py
+++ Assignment07/Src/Main.py
@@ -1,3 +1,16 @@
+#main.py
+
+# Name: Aanika Garre
+# email:garreaa@mail.uc.edu
+# Assignment Number: Assignment 07
+# Due Date: 2/29/24
+# Course/Section: IS 4010-001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This assignment focuses on modifying an existing project from GitHub.
+
+# Brief Description: This module goes over a basic understanding of using GitHub and how to upload and modify existing projects.
+# Citations:
+# Anything else that's relevant:
 '''
 Created on Feb 26, 2020
 
@@ -6,9 +19,8 @@ Created on Feb 26, 2020
 
 from Src.Assignment07 import generate_captcha
 
-result = generate_captcha()
+result = generate_captcha(7,"captcha.png")
 myCaptcha = result[0]
 captcha_string = result[1]
 print(">" + captcha_string + "<")
 myCaptcha.show()
-
